bddee5f749704ca4aee412cd077fabbdc65857a9
Kevo <customworldcompany@gmail.com>
2021-06-08 15:48:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Python mass YouTube video downloader
+
+### [!] This project is for educational purposes only!
+
+So, I was bored and made a bulk YouTube video downloaded that fetches YouTube video data from https://subscriptions.gir.st/
+
+1. First, paste YouTube video URLs/codes in video_urls.txt
+2. Then run download.py. All videos will be downloaded in the "saved_videos" directory (you might need to create it).
+
+**Requires:** [requests](https://pypi.org/project/requests/) & Python ^3.6 (tested on 3.9.1 Windows 10 64-bit & Ubuntu 20.4 LTS, I can't guarantee that this will work for you)
+
+- Licensed under "MIT" license. Feel free to change the source code, however crediting me or at least linking this repo is very appreciated.
---
+++ download.py
@@ -0,0 +1,100 @@
+import re
+import os
+import requests
+
+from typing import Callable, Generator, Tuple, Union, Dict, List
+
+from pathlib import Path
+ROOT_PATH = Path(__file__).parent.absolute()
+
+from dotenv import load_dotenv
+load_dotenv()
+
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY", None)
+
+
+def get_urls_from_playlist(playlist_url: str, save_to: Union[Path, str, bytes]=Path(f"{ROOT_PATH}/video_urls.txt")):
+    if not GOOGLE_API_KEY:
+        raise Exception("Missing 'GOOGLE_API_KEY' environment variable.")
+
+    json_data = requests.get()
+
+
+def __get_raw_urls(from_file: Union[Path, str, bytes]=Path(f"{ROOT_PATH}/video_urls.txt")) -> Generator[str, None, None]:
+    for line in open(from_file, "r"):
+        url = line.strip()
+
+        yield url
+
+
+def get_urls(from_file: Union[Path, str, bytes]=Path(f"{ROOT_PATH}/video_urls.txt"), json_details_url: str='https://subscriptions.gir.st/watch?v={code}&show=json') -> Generator[Tuple[str], None, None]:
+    youtube_regex = re.compile(r'^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$')
+
+    for url in __get_raw_urls(from_file=from_file):
+        match = re.match(youtube_regex, url)
+
+        if match:
+            code = match.group(5)
+
+            if code:
+                try:
+                    details = requests.get(json_details_url.format(code=code)).json() # type: dict
+
+                except Exception as e:
+                    print(f"Error: {e}")
+                    continue
+
+                title = details.get("videoDetails", dict()).get("title", code)
+                formats = details.get("streamingData", dict()).get("formats", list()) # type: List[Dict[Union[str, int]]]
+                highest_quality = max(formats, key=lambda x: x["width"])
+
+                if highest_quality:
+                    yield (title, highest_quality["url"])
+
+                else:
+                    continue
+
+            else:
+                continue
+
+        else:
+            continue
+
+
+def save_video(path: Union[Path, str, bytes], video_url: str):
+    request = requests.get(video_url, stream=True)
+
+
+    if not path.endswith(".mp4"):
+        raise TypeError("Video file name to save must end with '.mp4'.")
+
+    if os.path.exists(str(path)):
+        print(f"Not saving '{path}' because it already exists.")
+        return
+
+
+    video_file = open(path, 'wb')
+
+    for chunk in request.iter_content(100000, True):
+        video_file.write(chunk)
+
+    video_file.close()
+
+
+
+def download_all(urls: Callable=get_urls, *args, **kwargs):
+    for title, url in urls(*args, **kwargs):
+        path = Path(f"{ROOT_PATH}/saved_videos/{title}.mp4")
+
+        try:
+            save_video(path, url)
+
+        except Exception as e:
+                print(f"Error: {e}")
+                continue
+
+        print(f"Saved video '{title}' into {path}")
+
+
+if __name__ == "__main__":
+    download_all()
